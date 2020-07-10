3061dfcb16689a91afed36a4932b668da0ec84fd
hengxyz <mingzuheng@hotmail.com>
2020-07-10 23:12:13 +0200

    updated the Scopus2WOS.py and Scopus2WOS.exe which can process all the files *.ris under the folder.

--- Scopus2WOS.py
+++ Scopus2WOS.py
@@ -2,12 +2,19 @@
 import os
 import re
 #import time
+import glob
 
 def Scopus2HistCite():
     try:
         #files = glob.glob('woc.txt')
 
-        Scopus_file = 'scopus.ris'
+        #Scopus_file = 'scopus.ris'
+        Scopus_files = glob.glob('*.ris')
+        Lines_files = []
+        for Scopus_file in Scopus_files:
+            with open(Scopus_file, 'r') as Scopus:
+                for line in Scopus.readlines():
+                    Lines_files.append(line)
 
         if not os.path.isfile(Scopus_file):
             raise Exception("No file spcified")
@@ -28,26 +35,44 @@ def Scopus2HistCite():
         Lines_section = []
         read_section = False
         process = False
-        with open(Scopus_file, 'r') as Scopus:
-            for line in Scopus.readlines():
-                if line[:2] == 'TY':
-                    read_section = True
-                if line[:2] == 'ER':
-                    read_section = False
-                    process = False
-                if read_section:
-                    Lines_section.append(line)
-                else:
-                    if process == True:
-                        continue
-                    ## transforming the scopus to wos format:
-                    Lines_wos.append('PT J\n')
-                    ## TY
-                    lines_TY = [line for line in Lines_section if 'TY ' in line]
-                    doctype = lines_TY[0][6:-1]
+        # with open(Scopus_file, 'r') as Scopus:
+        #     for line in Scopus.readlines():
+        for line in Lines_files:
+            if line[:2] == 'TY':
+                read_section = True
+            if line[:2] == 'ER':
+                read_section = False
+                process = False
+            if read_section:
+                Lines_section.append(line)
+            else:
+                if process == True:
+                    continue
+                ## transforming the scopus to wos format:
+                Lines_wos.append('PT J\n')
+                ## TY
+                lines_TY = [line for line in Lines_section if 'TY ' in line]
+                doctype = lines_TY[0][6:-1]
 
-                    ## AU
-                    lines_AU = [line for line in Lines_section if 'AU ' in line]
+                ## AU
+                lines_AU = [line for line in Lines_section if 'AU ' in line]
+                if lines_AU:
+                    name = lines_AU[0][6:]
+                    name = name.replace('.', '')
+                    if ',' in name:
+                        strs_name = name.split(',')
+                        name0 = strs_name[0]
+                        if ' ' in name0:
+                            name0 = name0.replace(' ','')
+                            name = name0+','+strs_name[1]
+                    else:
+                        print('%s'%name)
+                    Lines_wos.append('AU %s'%name)
+                    for line in lines_AU[1:]:
+                        line = line.replace('.', '')
+                        Lines_wos.append(' %s'%line[6:])
+                else:
+                    lines_AU = [line for line in Lines_section if 'A2 ' in line]
                     if lines_AU:
                         name = lines_AU[0][6:]
                         name = name.replace('.', '')
@@ -55,97 +80,81 @@ def Scopus2HistCite():
                             strs_name = name.split(',')
                             name0 = strs_name[0]
                             if ' ' in name0:
-                                name0 = name0.replace(' ','')
-                                name = name0+','+strs_name[1]
+                                name0 = name0.replace(' ', '')
+                                name = name0 + ',' + strs_name[1]
                         else:
-                            print('%s'%name)
+                            print('%s' % name)
                         Lines_wos.append('AU %s'%name)
                         for line in lines_AU[1:]:
                             line = line.replace('.', '')
                             Lines_wos.append(' %s'%line[6:])
                     else:
-                        lines_AU = [line for line in Lines_section if 'A2 ' in line]
-                        if lines_AU:
-                            name = lines_AU[0][6:]
-                            name = name.replace('.', '')
-                            if ',' in name:
-                                strs_name = name.split(',')
-                                name0 = strs_name[0]
-                                if ' ' in name0:
-                                    name0 = name0.replace(' ', '')
-                                    name = name0 + ',' + strs_name[1]
-                            else:
-                                print('%s' % name)
-                            Lines_wos.append('AU %s'%name)
-                            for line in lines_AU[1:]:
-                                line = line.replace('.', '')
-                                Lines_wos.append(' %s'%line[6:])
-                        else:
-                            Lines_wos.append('AU\n')
+                        Lines_wos.append('AU\n')
 
 
-                    ## TI
-                    lines_TI = [line for line in Lines_section if 'TI ' in line]
-                    Lines_wos.append('TI %s' % lines_TI[0][6:])
-                    if lines_TI[0][6:] == 'Non-intrusive liveness detection by face images\n':
-                        print('stop')
-                    ## SO
-                    if doctype == 'JOUR' or doctype == 'CHAP':
-                        lines_T2 = [line for line in Lines_section if 'T2 ' in line]
+                ## TI
+                lines_TI = [line for line in Lines_section if 'TI ' in line]
+                Lines_wos.append('TI %s' % lines_TI[0][6:])
+                if lines_TI[0][6:] == 'Non-intrusive liveness detection by face images\n':
+                    print('stop')
+                ## SO
+                if doctype == 'JOUR' or doctype == 'CHAP':
+                    lines_T2 = [line for line in Lines_section if 'T2 ' in line]
+                    Lines_wos.append('SO %s' % lines_T2[0][6:])
+                else:
+                    lines_T2 = [line for line in Lines_section if 'C3 ' in line]
+                    if lines_T2:
                         Lines_wos.append('SO %s' % lines_T2[0][6:])
                     else:
-                        lines_T2 = [line for line in Lines_section if 'C3 ' in line]
+                        lines_T2 = [line for line in Lines_section if 'T2 ' in line]
                         if lines_T2:
                             Lines_wos.append('SO %s' % lines_T2[0][6:])
                         else:
-                            lines_T2 = [line for line in Lines_section if 'T2 ' in line]
-                            if lines_T2:
-                                Lines_wos.append('SO %s' % lines_T2[0][6:])
-                            else:
-                                Lines_wos.append('SO\n')
+                            Lines_wos.append('SO\n')
 
-                    ## LA
-                    lines_LA = [line for line in Lines_section if 'LA ' in line]
-                    Lines_wos.append('LA %s' % lines_LA[0][6:])
+                ## LA
+                lines_LA = [line for line in Lines_section if 'LA ' in line]
+                Lines_wos.append('LA %s' % lines_LA[0][6:])
 
-                    ## DT
-                    lines_DT = [line for line in Lines_section if 'TY ' in line]
-                    Lines_wos.append('DT %s' % lines_DT[0][6:])
+                ## DT
+                lines_DT = [line for line in Lines_section if 'TY ' in line]
+                Lines_wos.append('DT %s' % lines_DT[0][6:])
 
-                    ## DE %KEYWORDS
-                    lines_KW = [line for line in Lines_section if 'KW ' in line]
-                    strkw = 'DE'
-                    for line in lines_KW:
-                        strkw += ' %s;'%line[6:-1]
-                    Lines_wos.append('%s\n'%strkw)
+                ## DE %KEYWORDS
+                lines_KW = [line for line in Lines_section if 'KW ' in line]
+                strkw = 'DE'
+                for line in lines_KW:
+                    strkw += ' %s;'%line[6:-1]
+                Lines_wos.append('%s\n'%strkw)
 
-                    ## AB
-                    lines_AB = [line for line in Lines_section if 'AB ' in line]
-                    if lines_AB:
-                        Lines_wos.append('AB %s' % lines_AB[0][6:])
-                    else:
-                        Lines_wos.append('AB\n')
+                ## AB
+                lines_AB = [line for line in Lines_section if 'AB ' in line]
+                if lines_AB:
+                    Lines_wos.append('AB %s' % lines_AB[0][6:])
+                else:
+                    Lines_wos.append('AB\n')
 
-                    ## C1 %Affliation
-                    if 'AD' in line:
-                        lines_C1 = [line for line in Lines_section if 'AD ' in line]
-                        Lines_wos.append('C1 %s'%lines_C1[0][6:])
-                        for line in lines_C1[1:]:
-                            Lines_wos.append(' %s'%line[6:])
-                    else:
-                        Lines_wos.append('C1\n')
+                ## C1 %Affliation
+                if 'AD' in line:
+                    lines_C1 = [line for line in Lines_section if 'AD ' in line]
+                    Lines_wos.append('C1 %s'%lines_C1[0][6:])
+                    for line in lines_C1[1:]:
+                        Lines_wos.append(' %s'%line[6:])
+                else:
+                    Lines_wos.append('C1\n')
 
-                    ## RP
-                    Lines_wos.append('RP\n')
+                ## RP
+                Lines_wos.append('RP\n')
 
-                    ## CR %Reference
-                    idx_CR = 0
-                    lines_CR = []
-                    for i, line in enumerate(Lines_section):
-                        if 'References' in line:
-                            idx_CR = i
-                            lines_CR.append(line[18:])
-                            break
+                ## CR %Reference
+                idx_CR = 0
+                lines_CR = []
+                for i, line in enumerate(Lines_section):
+                    if 'References' in line:
+                        idx_CR = i
+                        lines_CR.append(line[18:])
+                        break
+                if idx_CR:
                     for line in Lines_section[idx_CR+1:]:
                         if 'UR ' in line and line.index('UR ')>2:
                             line = line[:line.index('UR ')]+'\n'
@@ -203,6 +212,8 @@ def Scopus2HistCite():
                                         docname = line[idx_docname + 6:idx_docname + 6+idx]
                                     else:
                                         docname = ''
+                        else:
+                            docname = ''
                         if docname:
                             docname = docname.replace(',', '')
                         ## volumen
@@ -276,81 +287,81 @@ def Scopus2HistCite():
                         line[4] = line[4].replace(' ', '')
                         Lines_wos.append('   %s, %s, %s, V%s, P%s, %s\n'%(line[0],line[1],line[2],line[3], line[4], line[5]))
 
-                    ## NR
-                    Lines_wos.append('NR\n')
+                ## NR
+                Lines_wos.append('NR\n')
 
-                    ## TC
-                    lines_TC = [line for line in Lines_section if 'N1  - ' in line]
-                    lines_TC = [line for line in lines_TC if 'Cited ' in line]
-                    if lines_TC:
-                        Lines_wos.append('TC %s' % lines_TC[0][16:])
-                    else:
-                        Lines_wos.append('TC\n')
+                ## TC
+                lines_TC = [line for line in Lines_section if 'N1  - ' in line]
+                lines_TC = [line for line in lines_TC if 'Cited ' in line]
+                if lines_TC:
+                    Lines_wos.append('TC %s' % lines_TC[0][16:])
+                else:
+                    Lines_wos.append('TC\n')
 
-                    ## SN
-                    lines_SN = [line for line in Lines_section if 'SN  - ' in line]
-                    if lines_SN:
-                        Lines_wos.append('SN %s' % lines_SN[0][6:])
-                    else:
-                        Lines_wos.append('SN\n')
-
-
-
-                    ## J9
-                    lines_J9 = [line for line in Lines_section if 'J2  - ' in line]
-                    if lines_J9:
-                        Lines_wos.append('J9 %s' % lines_J9[0][6:])
-                    else:
-                        Lines_wos.append('J9\n')
+                ## SN
+                lines_SN = [line for line in Lines_section if 'SN  - ' in line]
+                if lines_SN:
+                    Lines_wos.append('SN %s' % lines_SN[0][6:])
+                else:
+                    Lines_wos.append('SN\n')
 
 
-                    ## PY
-                    lines_PY = [line for line in Lines_section if 'PY  - ' in line]
-                    if lines_PY:
-                        Lines_wos.append('PY %s' % lines_PY[0][6:])
-                    else:
-                        Lines_wos.append('PY\n')
 
-                    ## VL
-                    lines_VL = [line for line in Lines_section if 'VL  - ' in line]
-                    if lines_VL:
-                        Lines_wos.append('VL %s' % lines_VL[0][6:])
-                    else:
-                        Lines_wos.append('VL\n')
+                ## J9
+                lines_J9 = [line for line in Lines_section if 'J2  - ' in line]
+                if lines_J9:
+                    Lines_wos.append('J9 %s' % lines_J9[0][6:])
+                else:
+                    Lines_wos.append('J9\n')
 
-                    ## IS
-                    lines_IS = [line for line in Lines_section if 'IS  -  ' in line]
-                    if lines_IS:
-                        Lines_wos.append('IS %s' % lines_IS[0][6:])
-                    else:
-                        Lines_wos.append('IS\n')
 
-                    ## BP
-                    lines_BP = [line for line in Lines_section if 'SP  - ' in line]
-                    if lines_BP:
-                        Lines_wos.append('BP %s' % lines_BP[0][6:])
-                    else:
-                        Lines_wos.append('BP\n')
+                ## PY
+                lines_PY = [line for line in Lines_section if 'PY  - ' in line]
+                if lines_PY:
+                    Lines_wos.append('PY %s' % lines_PY[0][6:])
+                else:
+                    Lines_wos.append('PY\n')
 
-                    ## EP
-                    lines_EP = [line for line in Lines_section if 'EP  - ' in line]
-                    if lines_EP:
-                        Lines_wos.append('EP %s' % lines_EP[0][6:])
-                    else:
-                        Lines_wos.append('EP\n')
+                ## VL
+                lines_VL = [line for line in Lines_section if 'VL  - ' in line]
+                if lines_VL:
+                    Lines_wos.append('VL %s' % lines_VL[0][6:])
+                else:
+                    Lines_wos.append('VL\n')
 
-                    ## DI
-                    lines_DI = [line for line in Lines_section if 'DO  - ' in line]
-                    if lines_DI:
-                        Lines_wos.append('DI %s' % lines_DI[0][6:])
-                    else:
-                        Lines_wos.append('DI\n')
+                ## IS
+                lines_IS = [line for line in Lines_section if 'IS  -  ' in line]
+                if lines_IS:
+                    Lines_wos.append('IS %s' % lines_IS[0][6:])
+                else:
+                    Lines_wos.append('IS\n')
 
-                    Lines_wos.append('ER\n')
-                    Lines_wos.append('\n')
-                    Lines_section = []
+                ## BP
+                lines_BP = [line for line in Lines_section if 'SP  - ' in line]
+                if lines_BP:
+                    Lines_wos.append('BP %s' % lines_BP[0][6:])
+                else:
+                    Lines_wos.append('BP\n')
 
-                    process = True
+                ## EP
+                lines_EP = [line for line in Lines_section if 'EP  - ' in line]
+                if lines_EP:
+                    Lines_wos.append('EP %s' % lines_EP[0][6:])
+                else:
+                    Lines_wos.append('EP\n')
+
+                ## DI
+                lines_DI = [line for line in Lines_section if 'DO  - ' in line]
+                if lines_DI:
+                    Lines_wos.append('DI %s' % lines_DI[0][6:])
+                else:
+                    Lines_wos.append('DI\n')
+
+                Lines_wos.append('ER\n')
+                Lines_wos.append('\n')
+                Lines_section = []
+
+                process = True
 
         with open('wos1.txt', 'w') as f:
             f.writelines(Lines_wos)
